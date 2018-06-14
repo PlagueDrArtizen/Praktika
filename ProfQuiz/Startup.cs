c0ffd5496e74e326d6cc7f578a160037b68ba257
olegk <olegk@DESKTOP-N5NED6I>
2018-06-14 22:24:56 +0300

    Initial commit

---
+++ ProfQuiz/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProfQuiz.Startup))]
+namespace ProfQuiz
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
