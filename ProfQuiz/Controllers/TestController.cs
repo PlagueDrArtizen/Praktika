800da4092f957297d621dd237a94b45d517f71ec
Bliznuk Igor <bliznukigor@outlook.com>
2018-06-17 15:28:42 +0300

    Start page implementation

---
+++ ProfQuiz/Controllers/TestController.cs
@@ -0,0 +1,39 @@
+﻿using ProfQuiz.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProfQuiz.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<ActionResult> TestStart()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> TestStart(TestViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Session.Add("TestState", viewModel);
+
+                // TODO: Заменить на переход на стриницу теста, когда она будет добавлена
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View(viewModel);
+            }
+        }
+    }
+}
---
+++ ProfQuiz/Models/TestVIewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProfQuiz.Models
+{
+    public class TestViewModel
+    {
+        [Required]
+        [Display(Name = "Имя")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Отчество")]
+        public string MiddleName { get; set; }
+
+        [Required]
+        [Display(Name = "Фамилия")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Индивидуальный номер")]
+        public string Identity { get; set; }
+    }
+}
